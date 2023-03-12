5ebccf6a699a014ee8d74713eaa6067f107e64de
Dheisom Gomes <dheisomgomes0@gmail.com>
2023-03-12 16:12:31 -0300

    Atualizando algumas coisinhas no website

---
+++ script.js
@@ -0,0 +1,17 @@
+const contacts = document.querySelector("div.contact");
+const link = (title, url, icon) => ({ title, url, icon });
+
+const links = [
+  link("Github", "https://github.com/dheisom", "logo-github"),
+  link("Instagram", "https://instagram.com/dheisomgomes", "logo-instagram"),
+  link("Telegram", "https://github.com/dheisom", "paper-plane-outline"),
+  link("Tabnews", "https://www.tabnews.com.br/dheisom", "folder-outline"),
+  link("E-mail", "mailto:dheisomgomes0@gmail.com", "mail-outline"),
+];
+
+links.map((item) => {
+  var element = `<a href="${item.url}" alt="${item.title}">`;
+  element += `<ion-icon name="${item.icon}" aria-hidden="true"></ion-icon>`;
+  element += `</a>`;
+  contacts.innerHTML += element;
+})
